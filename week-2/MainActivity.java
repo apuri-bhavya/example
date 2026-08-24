e772ae64d000a3d19f0e88af83758591e1ab43da
Apuri Bhavyasri <123apuribhavyasri@gmail.com>
2026-08-24 23:40:54 +0530

    Update MainActivity.java

--- week-2/MainActivity.java
+++ week-2/MainActivity.java
@@ -23,30 +23,21 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        // Initialize views
         editTextName = findViewById(R.id.editTextName);
         editTextContact = findViewById(R.id.editTextContact);
         editTextCity = findViewById(R.id.editTextCity);
 
         buttonSubmit = findViewById(R.id.buttonSubmit);
         textViewDisplay = findViewById(R.id.textViewDisplay);
-
-        // Set listener for button
         buttonSubmit.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
 
-                // Get input values
                 String name = editTextName.getText().toString().trim();
                 String contact = editTextContact.getText().toString().trim();
                 String city = editTextCity.getText().toString().trim();
-
-                // Display the values
-                String displayText =
-                        "Name: " + name +
-                        "\nContact: " + contact +
-                        "\nCity: " + city;
+                String displayText ="Name: " + name +"\nContact: " + contact +"\nCity: " + city;
 
                 textViewDisplay.setText(displayText);
             }
